4e0949518cb227c4a15ba60fcf2f45564ba2e495
Rune Myrland <rune@tellusr.com>
2025-06-19 13:06:28 +0200

    tweaks

---
+++ src/main/kotlin/framework/jsonpath/util/JsonToString.kt
@@ -0,0 +1,23 @@
+package com.tellusr.framework.jsonpath.util
+
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.contentOrNull
+import kotlin.collections.mapNotNull
+
+object JsonToString {
+    fun jsonToString(je: JsonElement): String? =
+        when (je) {
+            is JsonPrimitive -> je.contentOrNull
+            is JsonArray -> je.mapNotNull {
+                jsonToString(it)
+            }.joinToString("\n\n")
+
+            is JsonObject -> je.entries.joinToString("\n\n") {
+                "${it.key}: ${jsonToString(it.value)}"
+            }
+        }
+
+}
